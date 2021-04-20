a8a98b851019776d08f35842f9387eae4c9e2335
Sominemo <me@sominemo.com>
2021-04-21 00:17:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -55,9 +55,7 @@ $markup = [
 
 <head>
     <meta charset="UTF-8">
-    <link rel="preconnect" href="https://www.googletagmanager.com" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://badges.crowdin.net" />
     <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Primary Meta Tags -->
     <meta name="title" content="<?= __("title", UCOMP) ?>" data-lang="title">
